18616d8737c59d3f4e6442d1c63df96a630e3968
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-12-05 08:13:29 -0800

    plan of attack for Day 1

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,2 +1,4 @@
 import os
 # Do later, use dict for the list
+
+# Solution from teacher, download the text file (saves in /Day1/inputs) and split whenever it detects a space, rinse and repeat (and loop 1k times since there are 1k lines)
