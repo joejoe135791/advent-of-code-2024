5e763565ea324220de5992b303fd289117b22140
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-12-05 10:15:41 -0800

    got the beginning working

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,21 +1,31 @@
 import os
 import sys
-import termcolor
-currentWorkingDirectory = os.getcwd()
-textFile = f"{currentWorkingDirectory}/input/input.txt"
+from termcolor import cprint
+os.system('color')
+
+# currentWorkingDirectory = os.getcwd()
+# cprint(f"CWD: {currentWorkingDirectory}", "light_green")
+# currentCorrectDirectory = f"{currentWorkingDirectory}/Day1"
+textFile = f"C:/Users/jandino1921/Downloads/other/advent-of-code-2024/Day1/inputs/input.txt"
+cprint(f"text file: {textFile}", "light_green")
 inputList1 = []
 inputList2 =[]
 openInputFile = open(textFile, 'r')
 inputFileLines = openInputFile.readlines()
 # Solution from teacher, download the text file (saves in /Day1/inputs) and split whenever it detects a space, rinse and repeat (and loop 1k times since there are 1k lines)
 i = 0
-for i in range(1001):
+for i in range(1000):
     currentInputLine = inputFileLines[i]
     cprint(f"current Line: {currentInputLine}", "light_green")
     listReadyInput = currentInputLine.split()
-    inputList1 = listReadyInput[:1]
-    cprint(f"List 1 ready: {inputList1}", "light_green")
-    inputList2 = listReadyInput[:2]
-    cprint(f"List 1 ready: {inputList1}", "light_green")
+    listInput1 = listReadyInput[:1]
+    cprint(f"List 1 ready: {listInput1}", "light_green")
+    listInput2 = listReadyInput[1:]
+    cprint(f"List 2 ready: {listInput2}", "light_green")
+    inputList1.append(listInput1)
+    cprint(inputList1, "blue")
+    inputList1.append(listInput2)
+    cprint(inputList2, "red")
 
+input("Press Enter to close...")
 sys.exit("Finished, yipee")
