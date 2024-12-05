2ca93fe020aad16a226592cf22cbefb8c7eb1a7d
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-12-05 09:50:18 -0800

    oh god...

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,4 +1,21 @@
 import os
-# Do later, use dict for the list
-
+import sys
+import termcolor
+currentWorkingDirectory = os.getcwd()
+textFile = f"{currentWorkingDirectory}/input/input.txt"
+inputList1 = []
+inputList2 =[]
+openInputFile = open(textFile, 'r')
+inputFileLines = openInputFile.readlines()
 # Solution from teacher, download the text file (saves in /Day1/inputs) and split whenever it detects a space, rinse and repeat (and loop 1k times since there are 1k lines)
+i = 0
+for i in range(1001):
+    currentInputLine = inputFileLines[i]
+    cprint(f"current Line: {currentInputLine}", "light_green")
+    listReadyInput = currentInputLine.split()
+    inputList1 = listReadyInput[:1]
+    cprint(f"List 1 ready: {inputList1}", "light_green")
+    inputList2 = listReadyInput[:2]
+    cprint(f"List 1 ready: {inputList1}", "light_green")
+
+sys.exit("Finished, yipee")
